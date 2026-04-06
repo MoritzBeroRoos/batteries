43805e12c9d662ccd37e13268afba21b62db5d68
Moritz Roos macbook <mo.roos@gmx.de>
2026-04-06 20:17:11 +0200

    add Thomas Murrills to authors

--- Batteries/Tactic/Lint/TypeClass.lean
+++ Batteries/Tactic/Lint/TypeClass.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Gabriel Ebner, Moritz Roos
+Authors: Gabriel Ebner, Thomas Murrills, Moritz Roos
 -/
 module
 
